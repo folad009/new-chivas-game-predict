81699056a1beb1f9bf5738f7b14b6202055b46b9
Fola <folaremidixon@gmail.com>
2025-05-07 20:51:52 +0100

    update new

--- components/VideoBox.jsx
+++ components/VideoBox.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const VideoBox = () => (
   <section className='w-2/3 mx-auto -mt-[100px]'>
-    <video className="h-full w-full rounded-lg" controls muted>
+    <video className="h-full w-full rounded-lg" controls>
       <source src="https://www.w3schools.com/html/jobs9ru3.mp4" type="video/mp4" />
     </video>
 
